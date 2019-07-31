fca5174852c1233445aa91493e641b6fdb037871
Rebecca Forrester <bexforrester@eduroam-221-116.nomadic.bris.ac.uk>
2019-07-31 14:21:01 +0100

    updated code

--- hiretest.r
+++ hiretest.r
@@ -1,3 +1,6 @@
+# load HIRE
+library("HIREewas")
+
 ################################################################################################
 #Generate the EWAS data
 ################################################################################################
@@ -9,35 +12,49 @@ num <- length(alpha_vec)
 temp <- rgamma(num, shape = alpha_vec, rate = 1)
 return(temp / sum(temp))
 }
+#number of samples
 n <- 180
-n1 <- 60
-n2 <- 120
- #number of samples
-#number of controls
- #number of cases
+
+#assignment of discrete phenotype 
+
+#number of controls (1/3 of total)
+n1 <- (n/3)
+#number of cases (2/3 of total)
+n2 <- (2*(n/3))
+
 #################################################################################################
-# K=3
+# Generating cell lineages
 #################################################################################################
-m <- 2000   #number of CpG sites
+m <- 10000   #number of CpG sites
 K <- 3       #underlying cell type number
+
 #methylation profiles
 #assume cell type 1 and cell type 2 are from the same lineage
 #cell type 1
 methy1 <- rbeta(m,3,6)
-#cell type 2
+#cell type 2 (from the same lineage as cell type 1)
 methy2 <- methy1 + rnorm(m, sd=0.01)
 ind <- sample(seq_len(m), m/5)
 methy2[ind] <- rbeta(length(ind),3,6)
 #cell type 3
 methy3 <- rbeta(m,3,6)
 mu <- cbind(methy1, methy2, methy3)
+
+#################################################################################################
+#Discrete and continuous phenotypes
+#################################################################################################
+
 #number of covariates
 p <- 2
-#covariates / phenotype
+#covariates / phenotype (continuous phenotype age randomly drawn btw 20 and 50)
 X <- rbind(c(rep(0, n1),rep(1, n2)), runif(n, min=20, max=50))
+
+
 #set risk-CpG sites under each cell type for each phenotype
 beta <- array(0, dim=c(m,K,p))
-#control vs case
+
+#adding phenotype effects for case/control status (phenotype 1)
+# effect will be assigned as pos or neg effect with equal chance
 m_common <- 10
 max_signal <- 0.15
 min_signal <- 0.07
@@ -50,8 +67,10 @@ runif(m_seperate*2, min=min_signal, max=max_signal)
 signs <- sample(c(-1,1), m_seperate, replace=TRUE)
 beta[m_common+m_seperate+(seq_len(m_seperate)),K,1] <- signs *
 runif(m_seperate, min=min_signal, max=max_signal)
-#age
-base <- 20
+
+
+#age (bases 21 to 40) <<<<<<<<<<< check this part
+base <- 20 # should be 21?
 m_common <- 10
 
 max_signal <- 0.015
@@ -66,7 +85,20 @@ runif(m_seperate*2, min=min_signal, max=max_signal)
 signs <- sample(c(-1,1), m_seperate, replace=TRUE)
 beta[base+m_common+m_seperate+seq_len(m_seperate),seq_len(K),2] <- signs *
 runif(m_seperate, min=min_signal, max=max_signal)
+
+
 #generate the cellular compositions
+
+# assign dirichlet distribution based on size of K 
+
+#ddist_3cont <- (4, 4, 2+X[2,i]/10)
+#ddist_3case <- (4, 4, 5+X[2,i]/10)
+#ddist_5cont <- (3, 3, 3, 3, 2+X[2,i]/10)
+#ddist_5case <- (3, 3, 3, 3, 5+X[2,i]/10)
+#ddist_7cont <- (1, 3, 3, 3, 2, 2, 2+X[2,i]/10)
+#ddist_7case <- (1, 3, 3, 3, 2, 2, 5+X[2,i]/10)
+
+
 P <- vapply(seq_len(n), function(i){
 if(X[1,i]==0){ #if control
 rDirichlet(c(4,4, 2+X[2,i]/10))
@@ -98,16 +130,24 @@ ret_list <- HIRE(Ometh, X, num_celltype=K)
 
 #case vs control
 #Visualize the association pattern with the case/control status in the first 100 CpG sites 
+#write output to pdf
+pdf("casecontrolstatus.pdf")
 riskCpGpattern(ret_list$pvalues[seq_len(100), c(2,1,3)],
 main_title="Detected association pattern\n with disease status", hc_row_ind = FALSE) #c(2,1,3) was used because of the label switching
+dev.off()
 #age
 #Visualize the association pattern with the age in the first 100 CpG sites
+#write output to pdf
+pdf("age.pdf")
 riskCpGpattern(ret_list$pvalues[seq_len(100), K+c(2,1,3)],
     main_title="Detected association pattern\n with age", hc_row_ind = FALSE)
-
+dev.off()
 
 #a p-value matrix from the uniform distribution
+#write output to pdf
     pvalues <- matrix(runif(600), 100, 6)
     #Visualize this p-value matrix
+    pdf("pvalues.pdf")
     riskCpGpattern(pvalues,
     main_title="An example", hc_row_ind = FALSE)
+    dev.off()
