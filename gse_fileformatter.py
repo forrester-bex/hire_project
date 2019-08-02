cd28fa1f7fdfb9dd52db73400f9c0c073ef6fa97
Rebecca Forrester <bexforrester@eduroam-221-116.nomadic.bris.ac.uk>
2019-08-02 16:25:31 +0100

    further formatting code

--- gse_fileformatter.py
+++ gse_fileformatter.py
@@ -16,11 +16,54 @@ with open(inputfile, 'r') as inputf:
 meth_list = []
 
 #formatting the data into a dataframe
+import pandas
 with open('GSE42861_meth.txt', 'r') as meth_data:
 		for line in meth_data:
 		# remove start and end lines
 			if not line.startswith('!'):
 				# split line by tabs
 				splitlist = line.strip('\n').split('\t')
+				# remove empty lines
+				if len(splitlist) > 1:
 				#append to new list in memory
-				meth_list.append(splitlist)
+					meth_list.append(splitlist)
+
+
+# column names are row 1, ignoring first entry which references title of index column
+column_names = meth_list[0][1:]
+col_names = []
+# strip of redundant ""
+for entry in column_names:
+	col_names.append(entry.strip("'").strip('"'))
+
+# create list of row names (column 1 of every row)
+row_names = []
+r_names = []
+for entry in meth_list[1:]:
+	row_names.append(entry[0])
+
+for entry in row_names:
+	r_names.append(entry.strip("'").strip('"'))
+
+# create matrix
+matrix = []
+for entry in meth_list[1:]:
+	newent = []
+	# add values as floats rather than strings
+	for value in entry[1:]:
+		newent.append(float(value))
+	matrix.append(newent)
+
+
+# create dataframe
+df = pandas.DataFrame(matrix, columns=column_names, index=row_names)
+
+# write to csv
+df.to_csv('RAmeth_matrix.csv')
+
+# drop columns GSM1051535 and GSM1051691
+del df['GSM1051535']
+del df['GSM1051691']
+
+# calculate mean score of CpG loci (axis=1 means compute mean or row rather than column)
+mean_scores = df.mean(axis=1)
