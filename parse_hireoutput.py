e7ad1bed048bf61d3db9bff794377a07f9ea42f4
Rebecca Forrester <bexforrester@eduroam-221-116.nomadic.bris.ac.uk>
2019-08-15 15:23:16 +0100

    changed kept cpgs to 100 most discriminative

--- parse_hireoutput.py
+++ parse_hireoutput.py
@@ -1,9 +1,11 @@
 import pandas 
+import glob
 
 df = pandas.read_csv("RA_HIRE_pvalues.csv")
 
 # add row names
 matrix = pandas.read_csv('RAmeth_matrix_linesremoved_10Kmostvar.csv')
+
 
 cpgs = matrix[matrix.columns[0]]
 
@@ -17,12 +19,47 @@ df = df.set_index("cpg")
 celltypes = [df[df.columns[0]], df[df.columns[1]], df[df.columns[2]], df[df.columns[3]],
 			 df[df.columns[4]], df[df.columns[5]]]
 
-sorted_dfs = []
-for celltype in celltypes:
-	sorted_celltype = celltype.sort_values(ascending=False)
-	sorted_dfs.append(sorted_celltype[:100])
+
+rankedlist = 'data_cleaned_v2.refactor.rankedlist.txt'
+ranked = []
+
+with open(rankedlist, 'r') as r:
+	for line in r:
+		cpg = line.strip('\n')
+		ranked.append(cpg)
+
+# create a list of the top 100 discriminatory cpgs
+top = ranked[:100]
+
+# convert dfs to csv files
+counter = 1
+for entry in celltypes:
+	entry.to_csv("celltype"+str(counter)+'.csv')
+	counter += 1
+
+# load csv files back in 
+
+filelist = glob.glob('celltype[1-6].csv')
+
+finaldata = []
+for entry in filelist:
+	datalines = []
+	with open(entry, 'r') as data:
+		for line in data:
+			split_line = line.strip('\n').split(',')
+			# if cpg in top 100 list, add to list
+			if split_line[0] in top:
+				datalines.append(split_line)
+	finaldata.append(datalines)
+
+# convert back to dfs and export to csvs
+finalframes = []
+for entry in finaldata:
+	newdf = pandas.DataFrame(entry)
+	newdf = newdf.set_index(0)
+	finalframes.append(newdf)
 
 counter = 1
-for entry in sorted_dfs:
-	entry.to_csv("celltype"+str(counter)+'.csv')
+for entry in finalframes:
+	entry.to_csv("celltype"+str(counter)+'top100.csv')
 	counter += 1
